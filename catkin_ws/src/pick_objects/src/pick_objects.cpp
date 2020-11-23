70f25177faef6bede1aea510b7b7f15267177e63
joestilin <jstilin@gmail.com>
2020-11-23 02:25:39 +0000

    Modified pick_objects node to handle multiple goals

--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -1,13 +1,14 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <string>
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv){
-  // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "simple_navigation_goals");
+
+
+void goto_goal(double x, double y, std::string goal_name) {
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -18,14 +19,15 @@ int main(int argc, char** argv){
   }
 
   move_base_msgs::MoveBaseGoal goal;
-
+  
   // set up the frame parameters
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 3.0;
-  goal.target_pose.pose.orientation.w = 0.0;
+  goal.target_pose.pose.position.x = x;
+  goal.target_pose.pose.position.y = y;
+  goal.target_pose.pose.orientation.w = 1.0;
 
    // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending goal");
@@ -36,9 +38,21 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ROS_INFO("Robot successfully reached %s\n", goal_name.c_str());
   else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    ROS_INFO("The base failed to reach goal");
+}
+
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "simple_navigation_goals");
+
+
+  goto_goal(6.0, 6.0, "pickup zone");
+  ros::Duration(5).sleep();  
+  goto_goal(2.0, -4.0, "dropoff zone");
+  ros::Duration(5).sleep();  
 
   return 0;
 }
