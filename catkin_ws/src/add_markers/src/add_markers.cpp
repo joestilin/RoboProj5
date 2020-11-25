80d2b2e99ece9f11849f3d4007d4f25b15fb4a3b
joestilin <jstilin@gmail.com>
2020-11-25 03:36:42 +0000

    Added functionality to add markers node

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -1,17 +1,18 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 
 
 class add_markers_pubsub {
 	public:
 		add_markers_pubsub() {
-			marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
-			marker_sub = n.subscribe("odom", 10, &add_markers_pubsub::callback, this);
+			marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+			amcl_pose_sub = n.subscribe("amcl_pose", 1, &add_markers_pubsub::amcl_pose_callback, this);			
 		}
 		
-		public place_marker(double x, double y) {
+		void place_marker(double x, double y) {
 			// acceptable odom position error from pickup and dropoff points
 			double tolerance = 0.5;
  
@@ -40,8 +41,8 @@ class add_markers_pubsub {
 			// Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
 
 		  
-			marker.pose.position.x = 6.0;
-			marker.pose.position.y = 6.0;
+			marker.pose.position.x = x;
+			marker.pose.position.y = y;
 
 			marker.pose.position.z = 0;
 			marker.pose.orientation.x = 0.0;
@@ -62,26 +63,23 @@ class add_markers_pubsub {
 
 			marker.lifetime = ros::Duration();
 
-			// marker_pub.publish(marker);
+			marker_pub.publish(marker);
+  }
 
-			marker.pose.position.x = 2.0;
-			marker.pose.position.y = -4.0;
-
-			// marker_pub.publish(marker);
-
-        }
-
-		void callback(const nav_msgs::Odometry::ConstPtr& msg) {
+		void amcl_pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
 			// process incoming odom data to determine if robot is at pickup and dropoff points
     		// test subscribe
-			ROS_INFO("%f\n", msg->pose.pose.position.x);
+			//ROS_INFO("%f\n", msg->pose.pose.position.x);
 			
 			place_marker(msg->pose.pose.position.x, msg->pose.pose.position.y);	
 		}
+
 	private:
 		ros::NodeHandle n;
 		ros::Publisher marker_pub;
-		ros::Subscriber marker_sub;
+		ros::Subscriber amcl_pose_sub;
+		double initial_x;
+		double initial_y;
 	};
 
 
