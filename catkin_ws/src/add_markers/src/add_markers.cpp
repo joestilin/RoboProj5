c9225aed5ce72478fc69d6a944aca9b65093b6e0
joestilin <jstilin@gmail.com>
2020-11-28 03:29:04 +0000

    Continued buildout of add markers node

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -2,17 +2,19 @@
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-
+#include <geometry_msgs/Vector3.h>
+#include <cmath>
 
 
 class add_markers_pubsub {
 	public:
 		add_markers_pubsub() {
 			marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-			amcl_pose_sub = n.subscribe("amcl_pose", 1, &add_markers_pubsub::amcl_pose_callback, this);			
+			marker_sub = n.subscribe("marker_position", 1, &add_markers_pubsub::marker_position_callback, this);
+			amcl_pose_sub = n.subscribe("amcl_pose", 1, &add_markers_pubsub::amcl_pose_callback, this);
 		}
 		
-		void place_marker(double x, double y) {
+		void place_marker(double x, double y, bool add) {
 			// acceptable odom position error from pickup and dropoff points
 			double tolerance = 0.5;
  
@@ -36,7 +38,13 @@ class add_markers_pubsub {
 			marker.type = shape;
 
 			// Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-			marker.action = visualization_msgs::Marker::ADD;
+			if (add) {
+				marker.action = visualization_msgs::Marker::ADD;
+			}
+			else {
+				marker.action = visualization_msgs::Marker::DELETE;
+			}
+
 
 			// Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
 
@@ -51,14 +59,14 @@ class add_markers_pubsub {
 			marker.pose.orientation.w = 1.0;
 
 			// Set the scale of the marker -- 1x1x1 here means 1m on a side
-			marker.scale.x = 1.0;
-			marker.scale.y = 1.0;
-			marker.scale.z = 1.0;
+			marker.scale.x = 0.5;
+			marker.scale.y = 0.5;
+			marker.scale.z = 0.5;
 
 			// Set the color -- be sure to set alpha to something non-zero!
-			marker.color.r = 0.0f;
-			marker.color.g = 1.0f;
-			marker.color.b = 0.0f;
+			marker.color.r = 0.5f;
+			marker.color.g = 0.0f;
+			marker.color.b = 1.0f;
 			marker.color.a = 1.0;
 
 			marker.lifetime = ros::Duration();
@@ -67,19 +75,41 @@ class add_markers_pubsub {
   }
 
 		void amcl_pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
-			// process incoming odom data to determine if robot is at pickup and dropoff points
-    		// test subscribe
-			//ROS_INFO("%f\n", msg->pose.pose.position.x);
+
+			double amcl_x = (double)msg->pose.pose.position.x;
+			double amcl_y = (double)msg->pose.pose.position.y;	
+			double tol = 0.2;
+			if (pickup) {
+				if (sqrt(pow(amcl_x - marker_x, 2) + pow(amcl_y - marker_y, 2)) > tol) {
+					place_marker(marker_x, marker_y, true);
+				}
+				else {
+					pickup = false;
+					place_marker(marker_x, marker_y, false);
+				}
+			}
+      else {
+				if (sqrt(pow(amcl_x - marker_x, 2) + pow(amcl_y - marker_y, 2)) < tol) {
+					place_marker(marker_x, marker_y, true);
+				}
+			}
 			
-			place_marker(msg->pose.pose.position.x, msg->pose.pose.position.y);	
+		}
+
+		// callback function 
+		void marker_position_callback(const geometry_msgs::Vector3::ConstPtr& msg) {
+			marker_x = (double)msg->x;
+			marker_y = (double)msg->y;	
 		}
 
 	private:
 		ros::NodeHandle n;
 		ros::Publisher marker_pub;
+		ros::Subscriber marker_sub;
 		ros::Subscriber amcl_pose_sub;
-		double initial_x;
-		double initial_y;
+		bool pickup = true;
+		double marker_x = 0.0;
+		double marker_y = 0.0;
 	};
 
 
--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -2,6 +2,7 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <string>
+#include <geometry_msgs/Vector3.h>
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -46,10 +47,43 @@ int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
   ros::init(argc, argv, "simple_navigation_goals");
 
+	ros::NodeHandle n;
 
-  goto_goal(6.0, 6.0, "pickup zone");
-  ros::Duration(5).sleep();  
-  goto_goal(2.0, -4.0, "dropoff zone");
+	// publish the location of the object on a topic
+	ros::Publisher marker_pub = n.advertise<geometry_msgs::Vector3>("marker_position", 1);
+	geometry_msgs::Vector3 marker_msg;
+	
+	// pickup and dropoff locations
+	double pickup_x = 6.0;
+	double pickup_y = 6.0;
+	double pickup_z = 0.0;
+  double dropoff_x = 2.0;
+  double dropoff_y = -4.0;
+	double dropoff_z = 0.0;
+
+	marker_msg.x = pickup_x;
+	marker_msg.y = pickup_y;
+	marker_msg.z = pickup_z;
+	
+	// block until there is a subscriber to the "marker_pub" topic
+	while (marker_pub.getNumSubscribers() < 1) {
+		// wait
+	}
+	
+	marker_pub.publish(marker_msg);
+
+	// go to the pickup location
+  goto_goal(pickup_x, pickup_y, "pickup zone");
+
+  ros::Duration(5).sleep();
+
+  // go to the dropoff location
+  goto_goal(dropoff_x, dropoff_y, "dropoff zone");
+	marker_msg.x = dropoff_x;
+	marker_msg.y = dropoff_y;
+	marker_msg.z = dropoff_z;
+  marker_pub.publish(marker_msg);
+
   ros::Duration(5).sleep();  
 
   return 0;
